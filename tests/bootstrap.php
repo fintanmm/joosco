7848fe6a996d7d1ed0a92aa419a423a203520b24
Fintan MacMahon <fintan.macmahon@gmail.com>
2015-11-19 16:10:20 +0000

    Renamed test folder. Updated bootstrap for joomla component/plugin dev.

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+$autoloadFile = __DIR__.'/../../vendor/autoload.php';
+if (!file_exists($autoloadFile)) {
+    throw new RuntimeException('Install dependencies to run phpunit.');
+}
+require_once $autoloadFile;
+
+defined('JPATH_COMPONENT') or define('JPATH_COMPONENT', dirname(__DIR__).'/../src/Component/');
+defined('JPATH_PLUGIN') or define('JPATH_PLUGIN', dirname(__DIR__).'/../src/plugin/');
+define('JOOSCO_STUBS', dirname(__DIR__).'/unit/stubs/');
