813d678787c05fc2349ad7bb9259367a8c2dd377
Fintan MacMahon <fintan.macmahon@gmail.com>
2015-11-19 15:41:11 +0000

    Added namespaces. Applied cs-fixer.

--- src/Lib/Alfresco/Service/Association.php
+++ src/Lib/Alfresco/Service/Association.php
@@ -17,41 +17,41 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
- * As a special exception to the terms and conditions of version 2.0 of 
- * the GPL, you may redistribute this Program in connection with Free/Libre 
- * and Open Source Software ("FLOSS") applications as described in Alfresco's 
- * FLOSS exception.  You should have recieved a copy of the text describing 
- * the FLOSS exception, and it is also available here: 
+ * As a special exception to the terms and conditions of version 2.0 of
+ * the GPL, you may redistribute this Program in connection with Free/Libre
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's
+ * FLOSS exception.  You should have recieved a copy of the text describing
+ * the FLOSS exception, and it is also available here:
  * http://www.alfresco.com/legal/licensing"
  */
 
+namespace Lib\Alfresco\Service;
+
 class Association extends BaseObject
 {
-	private $_from;
-	private $_to;
-	private $_type;
-	
-	public function __construct($from, $to, $type)
-	{
-		$this->_from = $from;
-		$this->_to = $to;
-		$this->_type = $type;	
-	}
-	
-	public function getFrom()
-	{
-		return $this->_from;
-	}
-	
-	public function getTo()
-	{
-		return $this->_to;
-	}
-	
-	public function getType()
-	{
-		return $this->_type;
-	}
-}
+    private $_from;
+    private $_to;
+    private $_type;
 
-?>
+    public function __construct($from, $to, $type)
+    {
+        $this->_from = $from;
+        $this->_to = $to;
+        $this->_type = $type;
+    }
+
+    public function getFrom()
+    {
+        return $this->_from;
+    }
+
+    public function getTo()
+    {
+        return $this->_to;
+    }
+
+    public function getType()
+    {
+        return $this->_type;
+    }
+}
