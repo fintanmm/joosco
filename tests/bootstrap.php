ce466b40161cf4d8ea65bba06a6d35ff287f4f7b
Fintan MacMahon <fintan.macmahon@gmail.com>
2015-11-20 11:15:08 +0000

    Fixed path case.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,5 +7,5 @@ if (!file_exists($autoloadFile)) {
 require_once $autoloadFile;
 
 defined('JPATH_COMPONENT') or define('JPATH_COMPONENT', dirname(__DIR__).'/../src/Component/');
-defined('JPATH_PLUGIN') or define('JPATH_PLUGIN', dirname(__DIR__).'/../src/plugin/');
+defined('JPATH_PLUGIN') or define('JPATH_PLUGIN', dirname(__DIR__).'/../src/Plugin/');
 define('JOOSCO_STUBS', dirname(__DIR__).'/unit/stubs/');
