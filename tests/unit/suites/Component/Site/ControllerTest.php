ab1f2edba586528dd9544e205bc18c302e3ee168
Fintan MacMahon <fintan.macmahon@gmail.com>
2015-11-20 11:04:39 +0000

    Added initial unit test files.

---
+++ tests/unit/suites/Component/Site/ControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @author: Fintan MacMahon
+ *
+ * @copyright   Copyright (C) 2015 Fintan MacMahon
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+namespace JooscoUnitTests\Views;
+
+use Joosco\UnitTests\Helpers\TestCase;
+
+/**
+ * Unit Test class for JooscoViewDefault.
+ */
+class ControllerTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $app = $this->joomlaReflection->getValue($this->view, 'app');
+
+        $this->assertEquals($this->app, $app);
+    }
+}
